eeab5b37fbc957dbba7da4d38c4bb665672b6a31
DESKTOP-UJO5UUV\DELL <yachita.poohping@mail.kmutt.ac.th>
2021-06-04 23:56:59 +0700

    homeworkjaa

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace myhomework_6_2
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int ddd, yyy;
+            Console.Write("Input your birthday : ");
+            ddd = int.Parse(Console.ReadLine());
+            Console.Write("Input your year of birth : ");
+            yyy = int.Parse(Console.ReadLine());
+
+            string mmm;
+            Console.Write("Input your mount of birth : ");
+            mmm = Console.ReadLine();
+
+            int year = 2564;
+            Console.WriteLine("Your age is : {0} years old",year - yyy);
+            int age;
+            Console.Write("Input your age : ");
+            age = int.Parse(Console.ReadLine());
+
+            while (year == age)
+            {
+                if (year == age)
+                {
+                    if (year >= 18)
+                    {
+                        Console.WriteLine("You can sign up.");
+                        string name, surname;
+                        Console.Write("Input your name : ");
+                        name = Console.ReadLine();
+                        Console.Write("Input your surnsme : ");
+                        surname = Console.ReadLine();
+                        Console.WriteLine("Ready to use");
+                    }
+                    else
+                    {
+                        Console.Write("Under age cannot apply for sign up.");
+                    }
+                }
+                else { }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
